4683ea340aa896a5d3dfbd980945b1c558531a9a
Nicolás Neira <neira.navarrete01@gmail.com>
2023-12-18 23:07:34 -0300

    adding ex 32 / StrStr

---
+++ src/exercises/32_strStr.py
@@ -0,0 +1,33 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        if len(needle) > len(haystack):
+            return -1
+        
+        left, right = 0, len(needle) 
+        
+        while right <= len(haystack):
+            if haystack[left:right] == needle:
+                return left
+            else:
+                left += 1
+                right +=1
+        
+        return -1
